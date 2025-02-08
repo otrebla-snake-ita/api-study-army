2d444c1a6abf0cb42779948b0a2851d500eb5875
otrebla_snake_ita <otrebl.snake@gmail.com>
2025-02-08 20:26:37 +0100

    Created base project architecture

---
+++ src/Army-Hub/Infrastructure/ArmyHub.Persistence.SqlServer/Context/ArmyHubDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyHub.Persistence.SqlServer.Context;
+
+internal class ArmyHubDbContext : DbContext
+{
+    public ArmyHubDbContext(DbContextOptions<ArmyHubDbContext> options) : base(options)
+    { }
+}
